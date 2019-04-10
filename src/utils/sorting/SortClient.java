79eb1764b872a32e07ac17bed04fd31e464c66c6
christopher.eckles <christopher.eckles@WH-S2113-S-06.ccac.org>
2019-04-10 15:50:31 -0400

    added client testing

--- src/utils/sorting/SortClient.java
+++ src/utils/sorting/SortClient.java
@@ -5,6 +5,7 @@
  */
 package utils.sorting;
 
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -14,33 +15,56 @@ import java.util.Random;
  */
 public class SortClient {
 
-    public static final int TEST_SIZE = 10000;
+    public static final int TEST_SIZE = 100;
 
     public static void main(String[] args) {
-        Long[] times = new Long[TEST_SIZE];
+        int[] bucketSortTimes = new int[TEST_SIZE];
+        int[] bubbleSortTimes = new int[TEST_SIZE];
+        int[] mergeSortTimes = new int[TEST_SIZE];
 
-        //       SorterUnitTests.runTest();
-//           callMyself(10);
-//        System.out.println("Creating a new sort utility");
+        
         Sorter su;
-//
-//        System.out.println("Turning on debugging");
-//        su.setDebug(true);
-//
-//        System.out.println("FIRST TEST......");
-//
-        su = new Sorter();
 
+        su = new Sorter();
         for (int i = 0; i < TEST_SIZE; i++) {
-            int[] array1 = getRandomArray(100);
-            int[] result1 = su.bucketSort(array1);
-            //printArray(array1);
-            //printArray(result1);
-            Long time = su.getRunTime()[1] - su.getRunTime()[0];
-            System.out.println("Time to process: " + time);
-            times[i] = time;
+            int[] array = getRandomArray(100);
+            int[] result;
+            
+            System.out.println("\n\n\n\n\nArray to process: ");
+            printArray(array);
+            
+            result = su.bubbleSort(array);
+            bubbleSortTimes[i] = (int)(su.getRunTime()[1] - su.getRunTime()[0]);
+            System.out.println("Time to process: " + bubbleSortTimes[i]);
+            System.out.println("Bubble Sorted ");
+            printArray(result);
+            
+            System.out.println("Array to process: ");
+            printArray(array);
+
+            result = su.bucketSort(array);
+            bucketSortTimes[i] = (int)(su.getRunTime()[1] - su.getRunTime()[0]);
+            System.out.println("Time to process: " + bucketSortTimes[i]);
+            System.out.println("Bucket Sorted ");
+            printArray(result);
+            
+            System.out.println("Array to process: ");
+            printArray(array);
+            
+            result = su.mergeSort(array);
+            mergeSortTimes[i] = (int)(su.getRunTime()[1] - su.getRunTime()[0]);
+            System.out.println("Time to process: " + mergeSortTimes[i]);
+            System.out.println("Merge Sorted ");
+            printArray(result);
+            
 
         }
+
+        
+        System.out.println("Bucket Sort Average: " + findAverageUsingStream(bucketSortTimes));
+        System.out.println("Bubble Sort Average: " + findAverageUsingStream(bubbleSortTimes));
+        System.out.println("Merge Sort Average: " + findAverageUsingStream(mergeSortTimes));
+
 
 //        for (int i = 0; i < TEST_SIZE; i++) {
 //            int[] array1 = getRandomArray(10000);
@@ -50,7 +74,6 @@ public class SortClient {
 //            times[i] = time;
 //
 //        }
-
     }
 
     public static void printArray(int[] array) {
@@ -79,6 +102,10 @@ public class SortClient {
             return randomArray;
         }
         return new int[0];
+    }
+
+    public static double findAverageUsingStream(int[] array) {
+        return Arrays.stream(array).average().orElse(Double.NaN);
     }
 
 }
