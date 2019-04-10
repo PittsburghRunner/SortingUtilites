538e34d5cdbd27761551396b862a6b3458d533fb
christopher.eckles <christopher.eckles@WH-S2113-S-06.ccac.org>
2019-04-10 16:34:24 -0400

    added debug boolean

--- src/utils/sorting/SortClient.java
+++ src/utils/sorting/SortClient.java
@@ -18,9 +18,10 @@ public class SortClient {
     public static final int TEST_SIZE = 10000;
 
     public static void main(String[] args) {
+        boolean debug = false;
         int[] bucketSortTimes = new int[TEST_SIZE];
         int[] bubbleSortTimes = new int[TEST_SIZE];
-        int[] mergeSortTimes = new int[TEST_SIZE];
+    //   int[] mergeSortTimes = new int[TEST_SIZE];
 
         
         Sorter su;
@@ -30,32 +31,34 @@ public class SortClient {
             int[] array = getRandomArray(1000);
             int[] result;
             
-            System.out.println("\n\n\n\n\n Position " + i + " Array to process: ");
+            if(debug)System.out.println("\n\n\n\n\n ");
+            System.out.println("Position " + i );
+            if(debug)System.out.println(" Array to process: ");
             printArray(array);
             
             result = su.bubbleSort(array);
             bubbleSortTimes[i] = (int)(su.getRunTime()[1] - su.getRunTime()[0]);
-            System.out.println("Time to process: " + bubbleSortTimes[i]);
-            System.out.println("Bubble Sorted ");
+            if(debug)System.out.println("Time to process: " + bubbleSortTimes[i]);
+           if(debug) System.out.println("Bubble Sorted ");
             printArray(result);
             
-            System.out.println("Array to process: ");
+            if(debug)System.out.println("Array to process: ");
             printArray(array);
 
             result = su.bucketSort(array);
             bucketSortTimes[i] = (int)(su.getRunTime()[1] - su.getRunTime()[0]);
-            System.out.println("Time to process: " + bucketSortTimes[i]);
-            System.out.println("Bucket Sorted ");
+            if(debug)System.out.println("Time to process: " + bucketSortTimes[i]);
+            if(debug)System.out.println("Bucket Sorted ");
             printArray(result);
             
-            System.out.println("Array to process: ");
-            printArray(array);
-            
-            result = su.mergeSort(array);
-            mergeSortTimes[i] = (int)(su.getRunTime()[1] - su.getRunTime()[0]);
-            System.out.println("Time to process: " + mergeSortTimes[i]);
-            System.out.println("Merge Sorted ");
-            printArray(result);
+//            System.out.println("Array to process: ");
+//            printArray(array);
+//            
+//            result = su.mergeSort(array);
+//            mergeSortTimes[i] = (int)(su.getRunTime()[1] - su.getRunTime()[0]);
+//            if(debug)System.out.println("Time to process: " + mergeSortTimes[i]);
+//            if(debug)System.out.println("Merge Sorted ");
+//            printArray(result);
             
 
         }
@@ -63,17 +66,9 @@ public class SortClient {
         
         System.out.println("Bucket Sort Average: " + findAverageUsingStream(bucketSortTimes));
         System.out.println("Bubble Sort Average: " + findAverageUsingStream(bubbleSortTimes));
-        System.out.println("Merge Sort Average: " + findAverageUsingStream(mergeSortTimes));
+//        System.out.println("Merge Sort Average: " + findAverageUsingStream(mergeSortTimes));
 
 
-//        for (int i = 0; i < TEST_SIZE; i++) {
-//            int[] array1 = getRandomArray(10000);
-//            int[] result1 = su.bubbleSort(array1);
-//            Long time = su.getRunTime()[1] - su.getRunTime()[0];
-//            System.out.println("Time to process: " + time);
-//            times[i] = time;
-//
-//        }
     }
 
     public static void printArray(int[] array) {
